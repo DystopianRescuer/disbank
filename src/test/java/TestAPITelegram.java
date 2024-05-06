911142a3ae5c358748e6449d72a0864ce285520c
mrtaichi <isaaniet@gmail.com>
2024-05-05 21:38:46 -0600

    Todavia nada

--- src/test/java/TestAPITelegram.java
+++ src/test/java/TestAPITelegram.java
@@ -1,2 +1,25 @@
+import com.rateroscoloniatesocongo.disbank.telegramservice.ControladorTelegram;
+import com.rateroscoloniatesocongo.disbank.telegramservice.VistaTelegram;
+
+/**
+ *  Pruebas unitarias para los metodos de la {@link com.rateroscoloniatesocongo.disbank.telegramservice.VistaTelegram}
+ *  y ControladorTelegram
+ *
+ *  1. TestSetTokenBot
+ *  2. TestRecibirActualizacion
+ *  3.
+ *  */
 public class TestAPITelegram {
+
+    VistaTelegram vistaTelegram;
+    ControladorTelegram controladorTelegram;
+
+    /**
+     *  Inicializa una vista ejemplo y recibe la instancia de {@link ControladorTelegram} para cada prueba
+     *
+     *  La vista ejemplo está codeada con un chatID a un usuario emulado
+     *  */
+    public TestAPITelegram(){
+
+    }
 }
