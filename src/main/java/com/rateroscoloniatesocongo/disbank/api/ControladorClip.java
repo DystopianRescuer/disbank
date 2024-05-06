07b2f42f63c563246d8068bca8407e4c82a1c153
DystopianRescuer <diegobravis20@gmail.com>
2024-05-06 15:13:11 -0600

    Config reader implemented

--- src/main/java/com/rateroscoloniatesocongo/disbank/api/ControladorClip.java
+++ src/main/java/com/rateroscoloniatesocongo/disbank/api/ControladorClip.java
@@ -1,18 +1,21 @@
 package com.rateroscoloniatesocongo.disbank.api;
 
 import com.rateroscoloniatesocongo.disbank.transacciones.Cobro;
+import com.rateroscoloniatesocongo.disbank.util.ConfigReader;
 
 public class ControladorClip {
 
     private static ControladorClip instance;
 
-    private ControladorClip(String token) {
+    private final String apikey;
 
+    private ControladorClip(String apikey) {
+        this.apikey = apikey;
     }
 
     public static ControladorClip getInstance() {
         if(instance == null) {
-            instance = new ControladorClip();
+            instance = new ControladorClip(ConfigReader.getField("clip.apikey"));
         }
         return instance;
     }
--- src/main/java/com/rateroscoloniatesocongo/disbank/util/ConfigReader.java
+++ src/main/java/com/rateroscoloniatesocongo/disbank/util/ConfigReader.java
@@ -1,10 +1,43 @@
 package com.rateroscoloniatesocongo.disbank.util;
 
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
 public class ConfigReader {
+
+    private static ConfigReader instance;
+
+    private Properties config;
     private ConfigReader() {
+        this.config = new Properties();
+        InputStream archivo;
+
+        // Carga el archivo de configutación
+        try {
+            // TODO ruta de config
+            archivo = new FileInputStream("");
+            config.load(archivo);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "No se encontró el archivo de configuración");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static String getField(String key) {
-        
+    public ConfigReader getInstance() {
+        if(instance == null) {
+            instance = new ConfigReader();
+        }
+        return instance;
+    }
+
+
+    public String getField(String key) {
+        return this.config.getProperty(key);
     }
 }
