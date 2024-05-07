6ec04f22dfef614837402c784918fb9a8f350867
MrTaiChi <isaaniet@gmail.com>
2024-05-07 11:50:56 -0600

    test : Comenzando pruebas unitarias

--- src/test/java/TestAPITelegram.java
+++ src/test/java/TestAPITelegram.java
@@ -1,5 +1,8 @@
 import com.rateroscoloniatesocongo.disbank.telegramservice.ControladorTelegram;
 import com.rateroscoloniatesocongo.disbank.telegramservice.VistaTelegram;
+import com.rateroscoloniatesocongo.disbank.telegramservice.excepciones.ConexionYaIniciadaException;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  *  Pruebas unitarias para los metodos de la {@link com.rateroscoloniatesocongo.disbank.telegramservice.VistaTelegram}
@@ -15,11 +18,27 @@ public class TestAPITelegram {
     ControladorTelegram controladorTelegram;
 
     /**
-     *  Inicializa una vista ejemplo y recibe la instancia de {@link ControladorTelegram} para cada prueba
+     *  Inicializa ControladorTelegram para cada prueba
      *
-     *  La vista ejemplo está codeada con un chatID a un usuario emulado
+     *  La vista ejemplo se construye con el numero guardado en NumeroTest.txt
+     *  Y el ControladorTelegram se construye con el token de la configuracion
+     *  de la aplicacion
      *  */
     public TestAPITelegram(){
+        controladorTelegram = ControladorTelegram.getInstance();
+    }
+
+    /**
+     * Para probar si el metodo setToken solo se puede usar una vez
+     */
+    @Test public void TestSetToken(){
+        String tokenBot = VistaTelegram.getTokenBot();
+        try{
+            VistaTelegram.setTokenBot("a");
+        }catch(Exception e){
+            Assert.assertTrue(e.getClass() == ConexionYaIniciadaException.class);
+        }
+        Assert.assertEquals(tokenBot, VistaTelegram.getTokenBot());
 
     }
 }
