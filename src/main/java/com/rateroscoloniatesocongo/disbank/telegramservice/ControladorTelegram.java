225e8490b1c57f09897205d90fbad88a9c121877
mrtaichi <isaaniet@gmail.com>
2024-05-07 16:34:34 -0600

    Ajuste al nuevo setup de ConfigReader

--- src/main/java/com/rateroscoloniatesocongo/disbank/telegramservice/ControladorTelegram.java
+++ src/main/java/com/rateroscoloniatesocongo/disbank/telegramservice/ControladorTelegram.java
@@ -67,7 +67,7 @@ public class ControladorTelegram {
             return instance;
       
         try{
-            instance = new ControladorTelegram(ConfigReader.getInstance().getField("telegram.token"));
+            instance = new ControladorTelegram(ConfigReader.getField("telegram.token"));
         }catch(ConexionYaIniciadaException e){
             //No hacer nada, no puede pasar
         }catch(ErrorEnConexionException e){
