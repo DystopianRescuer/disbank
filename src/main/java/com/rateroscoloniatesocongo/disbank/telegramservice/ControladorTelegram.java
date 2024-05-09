fe778e6e6cf2a08d22aaef1dfa5e1ea8d5eb18b0
mrtaichi <isaaniet@gmail.com>
2024-05-09 16:14:42 -0600

    Avances sobre el hilo daemon

--- src/main/java/com/rateroscoloniatesocongo/disbank/telegramservice/ControladorTelegram.java
+++ src/main/java/com/rateroscoloniatesocongo/disbank/telegramservice/ControladorTelegram.java
@@ -5,6 +5,8 @@ import com.rateroscoloniatesocongo.disbank.telegramservice.excepciones.ConexionY
 import com.rateroscoloniatesocongo.disbank.telegramservice.excepciones.ErrorEnConexionException;
 import com.rateroscoloniatesocongo.disbank.transacciones.GestorTransacciones;
 import com.rateroscoloniatesocongo.disbank.util.ConfigReader;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -31,12 +33,16 @@ import java.util.HashMap;
  * <p>
  * - tokenBot : El token del bot que se usará para enviar y recibir mensajes. Se utiliza principalmente
  * para inicializar el ambito estático de {@link VistaTelegram}
- * - chats : Un HashMap que relaciona Clientes con su respectiva VistaTelegram, para tener una forma rápida
- * de enviar mensajes a los Asociados
+ * - chats : Un HashMap que relaciona Asociados con su respectiva VistaTelegram, para tener una forma rápida
+ * de enviar mensajes a los Asociados desde partes del programa donde no nos interesa el chatID (practicamente el resto del programa)
+ * - chatIds : Otra relacion, ahora entre las chatIDs y los Asociados, nos sirve para que el hilo daemon sepa de quién proviene cada
+ * update que reciba, de manera rápida
  * - offset : La API de telegram ofrece una manera de recibir Updates sin repeticiones que se basa en esta
  * variable, inicializa en 0 y se va actualizando conforme el controlador recibe actualizaciones
  * - gestorTransacciones : El controlador guarda una referencia al gestor de transacciones, pues necesitamos
  * una comunicacion ambivalente entre ambos.
+ * - daemon : El hilo daemon encargado de regularmente conseguir las updates de la API de telegram. Para motivos de debug solamente,
+ * tiene su getter.
  *
  */
 public class ControladorTelegram {
@@ -46,8 +52,10 @@ public class ControladorTelegram {
     public final JSONObject getMe;
     private final String tokenBot;
     private HashMap<Asociado, VistaTelegram> chats;
+    private HashMap<String, Asociado> chatIds;
     private int offset;
     private GestorTransacciones gestorTransacciones;
+    private Thread daemon;
 
     /** Singleton */
     private ControladorTelegram(String tokenBot) throws ConexionYaIniciadaException, ErrorEnConexionException {
@@ -55,10 +63,17 @@ public class ControladorTelegram {
         getMe = VistaTelegram.setTokenBot(tokenBot);
         chats = new HashMap<>();
         offset = 0;
+        chatIds = new HashMap<>();
+        daemon = new DaemonTelegram(this);
+        daemon.start();
     }
+
+    //Metodos para el resto del backend
 
     /**
      *  Obtiene la instancia singleton del controlador. La crea con el token del ConfigReader si no existe
+
+     *  Metodo para el backend
      *
      *  @return la instancia singleton del controlador
      *
@@ -79,11 +94,16 @@ public class ControladorTelegram {
         return instance;
     }
 
-    private void generarNuevaTransaccion(double cobro, String tipoDeCobro){
-
-    }
-
-    public void enviarMensaje(Mensaje mensaje){
+    /**
+     *  Envia un mensaje con las especificaciones dadas desde el objeto Mensaje
+     *
+     *  Metodo para el backend
+     *
+     *
+     *  @throws ErrorEnConexionException si existe un error en la conexion a Telegram
+     *
+     *  */
+    public void enviarMensaje(Mensaje mensaje) throws ErrorEnConexionException{
 
     }
 
@@ -95,6 +115,106 @@ public class ControladorTelegram {
      */
     public JSONObject getGetMe() {
         return getMe;
+
+    }
+
+    //Metodos debug
+
+    public Thread getDaemon(){
+        return daemon;
+    }
+
+    //Metodos para el hilo daemon
+
+    /**
+     *  Genera una nueva transaccion con los detalles dados y la envía al GestorTransacciones para esperar su respuesta
+     *
+     *  @param cobro         cantidad monetaria a cobrar
+     *  @param tipoDeCobro   tipo de cobro a pedir (terminal o link)
+     *  @param asociado      el asociado al cual está vinculado el cobro (de quien proviene la solicitud)
+     *  */
+    protected void generarNuevaTransaccion(double cobro, String tipoDeCobro, Asociado asociado){
+
+    }
+
+    /**
+     *  Registra una nueva interaccion con un chatID que no se encuentra registrado en los chats actuales
+     *
+     *  Busca en la base de datos en busca del asociado vinculado al chatID, si no existe entonces intenta setearlo a algun
+     *  asociado pendiente de registro con BaseDatos.setChatID.
+     *  */
+    protected void registrarNuevaInteraccion(String chatID){
+
+    }
+
+
+    /**
+     *  Realiza el corte personal del asociado registrado al chatID.
+     *
+     *  Esto solamente significa, hasta el momento, que dejamos de guardar su VistaTelegram, por motivos de optimizacion
+     *  */
+    protected void cortePersonal(Asociado asociado){
+
+    }
+
+    /**
+     *  Le da la lista de comandos al usuario que la solicita
+     *
+     *  @param chatID que la solicita
+     *
+     *  */
+    protected void darComandos(Asociado asociado){
+
+    }
+
+    /**
+     *  Manda solicitud de ayuda del chatID que la solicita
+     *
+     *  @param chatID que la solicita
+     *  */
+    protected void pedirAyuda(Asociado asociado){
+
+    }
+
+    /**
+     *  Retorna un arreglo JSON con las actualizaciones recibidas del bot desde la ultima llamada al metodo
+     *
+     *  Está destinado para ser usado desde el hilo daemon encargado del controlador.
+     *
+     *  @return ArregloJSON con las actualizaciones recibidas del bot desde la ultima llamada al metodo
+     *
+     *  @throws ErrorEnConexionException si no se pudo establecer la conexión adecuadamente
+     *  */
+    protected JSONArray getUpdates() throws ErrorEnConexionException{
+        JSONArray respuesta;
+        respuesta = VistaTelegram.recibirActualizacion(offset);
+        JSONObject ultimoRecibido = respuesta.getJSONObject(respuesta.length()-1);
+        offset = ultimoRecibido.getInt("update_id");
+
+        return respuesta;
+    }
+
+    /**
+     *  Busca el asociado vinculado a ese chatID. Necesitamos que el hilo daemon acceda al resto de metodos con esta información
+     *
+     *  Está destinado para ser usado desde el hilo daemon del controlador
+     *
+     *  @return Asociado de la lista de chats que está vinculado a ese chatID
+     *  */
+    protected Asociado buscarAsociado(String chatID){
+
+    }
+
+    //Metodos privados
+
+    /**
+     *  Retorna la vista telegram vinculada al asociado.
+     *
+     *  @param asociado  El asociado a usar como criterio de busqueda
+     *
+     *  @return VistaTelegram de la lista de chats que está vinculado a ese Asociado
+     *  */
+    private VistaTelegram buscarVistaTelegram(Asociado asociado){
 
     }
 }
