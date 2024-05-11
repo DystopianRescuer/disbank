79577b946b426bb72b3c21655c68230d18bf3dac
mrtaichi <isaaniet@gmail.com>
2024-05-10 18:22:04 -0600

    Daemon terminado

--- src/main/java/com/rateroscoloniatesocongo/disbank/telegramservice/DaemonTelegram.java
+++ src/main/java/com/rateroscoloniatesocongo/disbank/telegramservice/DaemonTelegram.java
@@ -1,9 +1,13 @@
 package com.rateroscoloniatesocongo.disbank.telegramservice;
 
+import com.rateroscoloniatesocongo.disbank.modelo.Asociado;
 import com.rateroscoloniatesocongo.disbank.telegramservice.excepciones.ErrorEnConexionException;
+import com.rateroscoloniatesocongo.disbank.telegramservice.excepciones.SolicitudNoEncontradaException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Hilo daemon del controlador de Telegram
@@ -19,7 +23,7 @@ import org.json.JSONObject;
  * metodos y procedimientos correspondientes
  *
  * Atributos:
- * -
+ * -controlador : La instancia del controlador que estará recibiendo todas las ordenes a lo largo de la ejecución del programa
  */
 public class DaemonTelegram extends Thread{
 
@@ -50,14 +54,83 @@ public class DaemonTelegram extends Thread{
                 //Obteniendo chatID y texto del mensaje
                 String chatID = mensaje.getJSONObject("chat").getString("id");
                 String text = mensaje.getString("text");
-
+                try{
+                    actuar(chatID, text);
+                }catch (ErrorEnConexionException e){
+                    //TODO : IDEM;
+                }
 
             }
-
         }
     }
 
-    private void actuar(String chatID, String mensaje){
+    /**
+     *  Decide que hacer con el mensaje que llegó por medio de una Update en el run() previo
+     *  */
+    private void actuar(String chatID, String mensaje) throws ErrorEnConexionException{
+        Asociado asociado = controlador.buscarAsociado(chatID);
 
+        if(asociado == null){
+            if(!mensaje.equals("/start"))
+                return;
+            else{
+                if(mensaje.equals("marco")){
+                    new VistaTelegram(chatID).enviarMensaje("polo");
+                }
+                try{
+                    controlador.registrarNuevoAsociado(chatID);
+                }catch(SolicitudNoEncontradaException e){
+                    new VistaTelegram(chatID).enviarMensaje(ControladorTelegram.noRegistroPendiente);
+                }
+            }
+        }else{
+            switch(mensaje){
+                case "Iniciar ventas":
+                    controlador.registrarNuevaInteraccion(chatID);
+                    break;
+
+                case "Cerrar ventas":
+                    controlador.cortePersonal(asociado);
+                    break;
+
+                case  "Comandos":
+                    controlador.darComandos(asociado);
+                    break;
+
+                case "Necesito ayuda":
+                    controlador.pedirAyuda(asociado);
+                    break;
+
+                default:
+                    if(asociado.recibiendoAyuda){
+                        controlador.mensajeAyuda(asociado, mensaje);
+                    }else{
+                        String[] detallesCobro = buscarCobro(mensaje);
+                        if(detallesCobro != null){
+                            controlador.generarNuevaTransaccion(Double.parseDouble(detallesCobro[0]), detallesCobro[1], asociado);
+                            return;
+                        }
+
+                        new VistaTelegram(chatID).enviarMensaje(ControladorTelegram.instruccionNoReconocida);
+                    }
+            }
+
+        }
+
+    }
+
+    private String[] buscarCobro(String mensaje){
+        Pattern pattern = Pattern.compile("Cobrar \\$([0-9]+\\.[0-9]{2}) con (.*)");
+        Matcher matcher = pattern.matcher(mensaje);
+
+        // Si se encuentra la coincidencia, extraemos la cantidad y la terminal
+        if (matcher.find()) {
+            String cantidad = matcher.group(1); // Obtenemos el grupo que coincide con la cantidad numérica
+            String terminal = matcher.group(2); // Obtenemos el grupo que coincide con la parte "metodo de cobro"
+            return new String[]{cantidad, terminal};
+        } else {
+            // Si no se encuentra la coincidencia, retornamos null
+            return null;
+        }
     }
 }
